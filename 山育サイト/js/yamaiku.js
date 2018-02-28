c213e795cb00b96cb6e07ba3f743b5a8b96aeee4
daiyaebi <daiyaoil@icloud.com>
2018-02-28 22:08:43 +0900

    2/28

---
+++ 山育サイト/js/yamaiku.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("button").mouseover(function(){
+    $(".buttons1").css("background-color","green");
+}).mouseout(function(){
+    $(".buttons1").css("background-color","");
+});
+});
+
